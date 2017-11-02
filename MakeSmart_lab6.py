88e997cf9c40a080bc59d93d1ce6ca556e5a6d0e
Mac Doussias <macdd@me.com>
2017-11-02 16:16:58 -0700

    Update MakeSmart_lab6.py
    Just made a list of functions that need defined per lab#6.

--- MakeSmart_lab6.py
+++ MakeSmart_lab6.py
@@ -1,3 +1,41 @@
 # Team MakeSmart
 # Maco Doussias, Pavlos Papadonikolakis, Jake McGhee
+# Lab 6  
 
+#Warmup
+def removeRedEye():
+  """ This function removes the red-eye from  a pic """
+  """ TODO define function and decide if needs arguments.  Rename function if wanted """
+
+def crazyEye():
+  """ This function changes eye color to a crazy eye color """
+  """ TODO define function and decide if needs arguments.  Rename Function if wanted """
+  
+#Problem 1
+def makePicSepia():
+  """ Converts a picture to a sepia tone  """
+  """ TODO define function and decide if needs arguments.  Rename Function if wanted """
+ # TODO * in this case add a check to make sure the red does not go over 255 - 
+ # TODO if the resulting red is over 255, set it to 255... This is a requirement for the function
+
+  
+def BetterBnW():
+  """ Converts a picture to black and white """
+  """ TODO this function was defined previously in LAB#3.  Insert here """
+
+#Problem 2
+def Artify():
+  """ This function will "artify" a pic by manipulating the color ranges """
+  """ TODO define function and decide if needs arguments.  Function should be name this per LAB#6 instruction. """
+  """ TODO See lab 6 for color ranges """
+
+#Problem 3
+def chromakey():
+  """ Chroma-Key finds all pixels that are the same color as the backdrop and replaces them like a green screen effect. """
+  """ TODO define function and decide if needs arguments.  Function should be name this per LAB#6 instruction. """
+  """ TODO See lab 6 for color ranges
+  
+  
+  
+  
+  
